e8debffa53d4c305f0c696498907439c9b3a8dca
Mickaël Canévet <mickael.canevet@gmail.com>
2021-06-03 13:25:53 +0200

    Add outputs

--- tests/sks-exoscale/outputs.tf
+++ tests/sks-exoscale/outputs.tf
@@ -1,9 +1,23 @@
-output "kubeconfig" {
-  value     = module.cluster.kubeconfig
+output "base_domain" {
+  value = module.cluster.base_domain
+}
+
+output "argocd_auth_token" {
   sensitive = true
+  value     = module.cluster.argocd_auth_token
+}
+
+output "kubeconfig" {
+  sensitive = true
+  value     = module.cluster.kubeconfig
+}
+
+output "admin_password" {
+  sensitive = true
+  value     = module.cluster.admin_password
 }
 
 output "grafana_admin_password" {
-  value     = module.cluster.grafana_admin_password
   sensitive = true
+  value     = module.cluster.grafana_admin_password
 }
