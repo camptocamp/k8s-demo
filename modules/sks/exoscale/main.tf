779e4f8621619a21cbe8353c3a868fde3fcb8cb9
Mickaël Canévet <mickael.canevet@gmail.com>
2021-07-22 10:32:59 +0200

    feat: set smaller nodes for routers

--- modules/sks/exoscale/main.tf
+++ modules/sks/exoscale/main.tf
@@ -14,7 +14,7 @@ locals {
   default_nodepools = {
     "router-${var.cluster_name}" = {
       size          = 2
-      instance_type = "medium"
+      instance_type = "small"
     },
     "devops-stack-${var.cluster_name}" = {
       size          = 2
