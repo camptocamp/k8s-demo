800ed49067feeeac7d584216748fd8d57938f967
Mickaël Canévet <mickael.canevet@gmail.com>
2021-07-22 10:49:17 +0200

    fix: don't deploy ca-issuer

--- modules/sks/exoscale/main.tf
+++ modules/sks/exoscale/main.tf
@@ -162,12 +162,7 @@ module "argocd" {
   }
 
   app_of_apps_values_overrides = [
-    templatefile("${path.module}/values.tmpl.yaml",
-      {
-        root_cert = base64encode(tls_self_signed_cert.root.cert_pem)
-        root_key  = base64encode(tls_private_key.root.private_key_pem)
-      }
-    ),
+    templatefile("${path.module}/values.tmpl.yaml", {}),
     var.app_of_apps_values_overrides,
   ]
 
@@ -184,26 +179,4 @@ resource "random_password" "clientsecret" {
 resource "random_password" "admin_password" {
   length  = 16
   special = false
-}
-
-resource "tls_private_key" "root" {
-  algorithm = "ECDSA"
-}
-
-resource "tls_self_signed_cert" "root" {
-  key_algorithm   = "ECDSA"
-  private_key_pem = tls_private_key.root.private_key_pem
-
-  subject {
-    common_name  = "devops-stack.camptocamp.com"
-    organization = "Camptocamp, SA"
-  }
-
-  validity_period_hours = 8760
-
-  allowed_uses = [
-    "cert_signing",
-  ]
-
-  is_ca_certificate = true
 }
